dacf6198e454a5f34c563567e36e4a86ed7573cb
Siddarth Tamse <sidtamse7@gmail.com>
2024-07-03 20:24:23 +0530

    This is first get and post API

--- storeapi/main.py
+++ storeapi/main.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
-
+from storeapi.models.post import UserPost, UserPostIn
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello, world!"}
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello, world!"}
+
+
+# Our social media API: adding posts
+
+post_table = {}
+
+
+@app.post("/post", response_model=UserPost)
+async def create_post(post: UserPostIn):
+    data = post.dict()
+    last_record_id = len(post_table)
+    new_post = {**data, "id": last_record_id}
+    post_table[last_record_id] = new_post
+    return new_post
+
+
+@app.get("/post", response_model=list[UserPost])
+async def get_all_post():
+    return list(post_table.values())
+
