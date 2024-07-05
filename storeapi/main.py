6f7b9c2a8009f5132eaa3d0fb4e9efa1673926c1
Siddarth Tamse <sidtamse7@gmail.com>
2024-07-05 20:25:15 +0530

    create_comment,get_comment_on_post,get_post_with_comments,

--- storeapi/main.py
+++ storeapi/main.py
@@ -1,5 +1,12 @@
-from fastapi import FastAPI
-from storeapi.models.post import UserPost, UserPostIn
+
+from fastapi import FastAPI, HTTPException
+from storeapi.models.post import (
+    UserPost,
+    UserPostIn,
+    Comment,
+    CommentIn,
+    UserPostWithComment
+)
 app = FastAPI()
 
 
@@ -11,9 +18,14 @@ app = FastAPI()
 # Our social media API: adding posts
 
 post_table = {}
+comment_table = {}
 
 
-@app.post("/post", response_model=UserPost)
+def find_post(post_id: int):
+    return post_table.get(post_id)
+
+
+@app.post("/post", response_model=UserPost, status_code=201)
 async def create_post(post: UserPostIn):
     data = post.dict()
     last_record_id = len(post_table)
@@ -25,4 +37,39 @@ async def create_post(post: UserPostIn):
 @app.get("/post", response_model=list[UserPost])
 async def get_all_post():
     return list(post_table.values())
+
+
+@app.post("/comment", response_model=Comment)
+async def create_comment(comment: CommentIn):
+    post = find_post(comment.post_id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    data = comment.dict()
+    last_record_id = len(comment_table)
+    new_comment = {**data, "id": last_record_id}
+    comment_table[last_record_id] = new_comment
+    return new_comment
+
+
+@app.get("/post/{post_id}/comment", response_model=list[Comment])
+async def get_comment_on_post(post_id: int):
+    return [
+        comment for comment in comment_table.values() if comment["post_id"] == post_id
+    ]
+
+
+@app.get("/post/{post_id}", response_model=UserPostWithComment)
+async def get_post_with_comments(post_id: int):
+    post = find_post(post_id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+    return {
+        "post": post,
+        "comments": await get_comment_on_post(post_id)
+    }
+
+
+
+
 
