80a37d4dc2b4d5a00a76f52b72b934ebafa4a62b
Siddarth Tamse <sidtamse7@gmail.com>
2024-07-25 16:01:06 +0530

    async def test_authenticate_user_wrong_password(registered_user: dict): test_access_token_expire_minutes,test_create_access_token,test_authenticate_user,test_authenticate_user_not_found,

--- storeapi/tests/test_security.py
+++ storeapi/tests/test_security.py
@@ -1,6 +1,17 @@
 import pytest
-
+from jose import jwt
 from storeapi import security
+
+
+def test_access_token_expire_minutes():
+    assert security.access_token_expire_minutes() == 30
+
+
+def test_create_access_token():
+    token = security.create_access_token("123")
+    assert {"sub": "123"}.items() <= jwt.decode(
+        token, key=security.SECRETE_KEY, algorithms=[security.ALGORITHM]
+    ).items()
 
 
 def test_password_hashes():
@@ -19,3 +30,24 @@ async def test_get_user(registered_user: dict):
 async def test_get_user_not_found():
     user = await security.get_user("test@example.com")
     assert user is None
+
+
+@pytest.mark.anyio
+async def test_authenticate_user(registered_user: dict):
+    user = await security.authenticate_user(
+        registered_user["email"], registered_user["password"]
+    )
+    assert user.email == registered_user["email"]
+
+
+@pytest.mark.anyio
+async def test_authenticate_user_not_found():
+    with pytest.raises(security.HTTPException):
+        await security.authenticate_user("test@example.net", "123")
+
+
+@pytest.mark.anyio
+async def test_authenticate_user_wrong_password(registered_user: dict):
+    with pytest.raises(security.HTTPException):
+        await security.authenticate_user(registered_user["email"], "wrong password")
+
